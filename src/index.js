100183be4488535643318b729922399193a88b13
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2020-09-15 21:54:31 +0200

    first project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const TodoList = () => {
+    return (
+<ul>
+    <li>Learn React</li>
+    <li>Create Awesome App</li>
+</ul>
+    );
+};
+const AppHeader = () => {
+    return (
+        <h1>My Todo List</h1>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const SearchPanel = () => {
+    return (
+        <input placeholder="search"/>
+    )
+};
+const App = () => {
+    return (
+        <div>
+        <AppHeader/>
+        <SearchPanel/>
+        <TodoList/>
+        </div>
+    )
+};
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
